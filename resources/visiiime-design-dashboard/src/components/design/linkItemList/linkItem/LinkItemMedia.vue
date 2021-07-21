ea815823e6ccd7773141958a2da683db2e47cbcf
東東 <s8809880988@gmail.com>
2021-07-21 23:03:20 +0800

    dong update

--- resources/visiiime-design-dashboard/src/components/design/linkItemList/linkItem/LinkItemMedia.vue
+++ resources/visiiime-design-dashboard/src/components/design/linkItemList/linkItem/LinkItemMedia.vue
@@ -1,141 +1,113 @@
 <template>
-  <v-ob ref="vob" tag="div" class="p-2">
-    <select v-model="linkItem.mediaName">
-      <option value="youtube">youtube</option>
-      <option value="spotify">spotify</option>
-      <option value="appleP">apple podcasts</option>
-    </select>
-    <div class="flex justify-center align-middle">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5 mt-1 text-gray-600 fill-current cursor-pointer"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path
-          d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
-        />
-      </svg>
-      <v-p v-slot="{ errors }" rules="required" name="連結名稱" tag="div" class="w-full">
-        <input
-          v-model="linkItem.linkName"
-          :disabled="linkItem.online"
-          type="text"
-          class="w-full text-xs p-2 focus:border-red-100 outline-none"
-          autocomplete="off"
-          placeholder="連結名稱"
-          @blur="validate"
-          required
-        />
-        <div class="mb-5 w-full text-red-500 text-xs">{{ errors[0] }}</div>
-      </v-p>
-    </div>
-    <div class="flex justify-center align-middle mb-2">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5 mt-1 text-gray-600 fill-current cursor-pointer"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
-          clip-rule="evenodd"
-        />
-      </svg>
-      <v-p v-slot="{ errors }" :rules="`required|${mediaRule}`" name="網址" tag="div" class="w-full">
-        <input
-          @blur="validate"
-          v-model="linkItem.link"
-          :disabled="linkItem.online"
-          type="text"
-          class="mb-0 w-full text-xs p-2 focus:border-red-100 outline-none"
-          autocomplete="off"
-          placeholder="https://www.example.com"
-          required
-        />
-        <div class="w-full text-red-500 text-xs">{{ errors[0] }}</div>
-      </v-p>
-    </div>
-    
-    <label class="inline-flex items-center">
-      <input v-model="linkItem.mediaOpenType" value="EXT" type="radio" class="form-radio">
-      <span class="ml-2">外部連結</span>
-    </label>
-    <label class="inline-flex items-center ml-6">
-      <input v-model="linkItem.mediaOpenType" value="INR" type="radio" class="form-radio">
-      <span class="ml-2">內部開啟</span>
-    </label>
-  </v-ob>
-</template>
+    <v-ob ref="vob" tag="div" class="p-2">
+        <select v-model="linkItem.mediaName">
+            <option value="youtube">youtube</option>
+            <option value="spotify">spotify</option>
+            <option value="appleP">apple podcasts</option>
+            <option value="soundcloud">soundcloud</option>
+        </select>
+        <div class="flex justify-center align-middle">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mt-1 text-gray-600 fill-current cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
+                <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+            </svg>
+            <v-p v-slot="{ errors }" rules="required" name="連結名稱" tag="div" class="w-full">
+                <input v-model="linkItem.linkName" :disabled="linkItem.online" type="text" class="w-full text-xs p-2 focus:border-red-100 outline-none" autocomplete="off" placeholder="連結名稱" @blur="validate" required />
+                <div class="mb-5 w-full text-red-500 text-xs">{{ errors[0] }}</div>
+            </v-p>
+        </div>
+        <div class="flex justify-center align-middle mb-2">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mt-1 text-gray-600 fill-current cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
+                <path
+                    fill-rule="evenodd"
+                    d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
+                    clip-rule="evenodd"
+                />
+            </svg>
+            <v-p v-slot="{ errors }" :rules="`required|${mediaRule}`" name="網址" tag="div" class="w-full">
+                <input @blur="validate" v-model="linkItem.link" :disabled="linkItem.online" type="text" class="mb-0 w-full text-xs p-2 focus:border-red-100 outline-none" autocomplete="off" placeholder="https://www.example.com" required />
+                <div class="w-full text-red-500 text-xs">{{ errors[0] }}</div>
+            </v-p>
+        </div>
 
+        <label class="inline-flex items-center">
+            <input v-model="linkItem.mediaOpenType" value="EXT" type="radio" class="form-radio" />
+            <span class="ml-2">外部連結</span>
+        </label>
+        <label class="inline-flex items-center ml-6">
+            <input v-model="linkItem.mediaOpenType" value="INR" type="radio" class="form-radio" />
+            <span class="ml-2">內部開啟</span>
+        </label>
+    </v-ob>
+</template>
 
 <script>
 import LinkItemVO from "@/vo/design/linkItemList/LinkItemVO";
 import { ValidationProvider as VP } from "vee-validate";
 
 export default {
-  components: {
-    VP,
-  },
-  props: {
-    linkItem: {
-      type: LinkItemVO,
-      required: true,
+    components: {
+        VP,
     },
-    idx: {
-      type: Number,
-      required: true,
+    props: {
+        linkItem: {
+            type: LinkItemVO,
+            required: true,
+        },
+        idx: {
+            type: Number,
+            required: true,
+        },
     },
-  },
-  computed: {
-    modalName() {
-      return "linkItem" + this.idx;
+    computed: {
+        modalName() {
+            return "linkItem" + this.idx;
+        },
+        mediaRule() {
+            let rule = "";
+            switch (this.linkItem.mediaName) {
+                case "spotify":
+                    rule = "spotifyUrl";
+                    break;
+                case "appleP":
+                    rule = "applePUrl";
+                    break;
+                case "soundcloud":
+                    rule = "soundcloudUrl";
+                    break;
+                default:
+                    rule = "youtubeUrl";
+                    break;
+            }
+            return rule;
+        },
     },
-    mediaRule() {
-      let rule = '';
-      switch (this.linkItem.mediaName) {
-        case 'spotify':
-          rule = 'spotifyUrl'
-          break;
-        case 'appleP':
-          rule = 'applePUrl'
-          break;
-        default:
-          rule = 'youtubeUrl'
-          break;
-      }
-      return rule;
-    }
-  },
-  methods: {
-    async validate() {
-      const result = await this.$refs.vob.validate();
-      if (!result) {
-        // post api
-        this.linkItem.online = false;
-        this.linkItem.valid = false;
-      } else {
-        this.linkItem.valid = true;
-      }
+    methods: {
+        async validate() {
+            const result = await this.$refs.vob.validate();
+            if (!result) {
+                // post api
+                this.linkItem.online = false;
+                this.linkItem.valid = false;
+            } else {
+                this.linkItem.valid = true;
+            }
+        },
+        updateImage(imageUrl) {
+            this.linkItem.thumbnail = imageUrl;
+        },
+        openUploadImageForm() {
+            this.$modal.show(this.modalName);
+        },
     },
-    updateImage(imageUrl) {
-      this.linkItem.thumbnail = imageUrl;
+    watch: {
+        "linkItem.online"() {
+            this.validate();
+        },
     },
-    openUploadImageForm() {
-      this.$modal.show(this.modalName);
+    mounted() {
+        this.validate();
     },
-  },
-  watch: {
-    "linkItem.online"() {
-      this.validate();
-    },
-  },
-  mounted() {
-    this.validate();
-  },
 };
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
--- resources/visiiime-design-dashboard/src/components/themes/VBasic/parts/MediaWindow.vue
+++ resources/visiiime-design-dashboard/src/components/themes/VBasic/parts/MediaWindow.vue
@@ -1,96 +1,98 @@
 <template>
-  <div v-if="mediaWindowOpen" @click.stop="closeWindow" class="media-modal">
-    <div class="window-wrapper">
-      <div class="window" v-html="iframe"></div>
+    <div v-if="mediaWindowOpen" @click.stop="closeWindow" class="media-modal">
+        <div class="window-wrapper">
+            <div class="window" v-html="iframe"></div>
+        </div>
     </div>
-  </div>
 </template>
 <script>
 export default {
-  props: {
-    link: {
-      type: String,
-      required: true,
+    props: {
+        link: {
+            type: String,
+            required: true,
+        },
+        mediaName: {
+            type: String,
+            required: true,
+        },
     },
-    mediaName: {
-      type: String,
-      required: true,
-    }
-  },
-  computed: {
-    mediaWindowOpen() {
-      return this.link !== '' && this.mediaName !== '';
+    computed: {
+        mediaWindowOpen() {
+            return this.link !== "" && this.mediaName !== "";
+        },
+        iframe() {
+            let iframe = "";
+            if (this.mediaName === "youtube") {
+                const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+                const match = this.link.match(regExp);
+                let youtubeId;
+                if (match && match[2].length == 11) {
+                    youtubeId = match[2];
+                } else {
+                    youtubeId = "error";
+                }
+                iframe = `<iframe src="//www.youtube.com/embed/${youtubeId}?autoplay=1" frameborder="0" allowfullscreen></iframe>`;
+            } else if (this.mediaName === "spotify") {
+                let iLink = this.link.replace("open.spotify.com", "open.spotify.com/embed");
+
+                iframe = `<iframe src="${iLink}" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
+            } else if (this.mediaName === "appleP") {
+                let ilink;
+                if (this.link.indexOf("podcasts") !== -1) {
+                    ilink = this.link.replace("podcasts", "embed.podcasts");
+                }
+                if (this.link.indexOf("itunes") !== -1) {
+                    ilink = this.link.replace("itunes", "embed.itunes");
+                }
+                console.log(ilink);
+
+                iframe = `<iframe src="${ilink}" frameborder="0" height="400" allowtransparency="true" allow="encrypted-media"></iframe>`;
+            } else if (this.mediaName === "soundcloud") {
+                let iLink = this.link.replace("soundcloud.com/", "w.soundcloud.com/player/?url=https://soundcloud.com/");
+                iframe = `<iframe src="${iLink}" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
+            }
+
+            return iframe;
+        },
     },
-    iframe() {
-      let iframe = "";
-      if (this.mediaName === "youtube") {
-        const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
-        const match = this.link.match(regExp);
-        let youtubeId;
-        if (match && match[2].length == 11) {
-          youtubeId = match[2];
-        } else {
-          youtubeId = "error";
-        }
-        iframe = `<iframe src="//www.youtube.com/embed/${youtubeId}?autoplay=1" frameborder="0" allowfullscreen></iframe>`;
-      } else if (this.mediaName === "spotify") {
-        let iLink = this.link.replace("open.spotify.com", "open.spotify.com/embed");
-
-        iframe = `<iframe src="${iLink}" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
-      } else if (this.mediaName === "appleP") {
-        let ilink;
-        if (this.link.indexOf("podcasts") !== -1) {
-          ilink = this.link.replace("podcasts", "embed.podcasts");
-        }
-        if (this.link.indexOf("itunes") !== -1) {
-          ilink = this.link.replace("itunes", "embed.itunes");
-        }
-        console.log(ilink);
-
-        iframe = `<iframe src="${ilink}" frameborder="0" height="400" allowtransparency="true" allow="encrypted-media"></iframe>`;
-      }
-
-      return iframe;
-    }
-  },
-  methods: {
-    closeWindow() {
-      this.$emit('close-media')
-    }
-  },
+    methods: {
+        closeWindow() {
+            this.$emit("close-media");
+        },
+    },
 };
 </script>
 <style lang="scss">
 .media-modal {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background: rgba(#000, 0.5);
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  transition: .3s;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  .window-wrapper {
+    position: absolute;
     width: 100%;
-    max-width: 576px;
-    padding: 0 5px;
-  }
-  .window {
-    position: relative;
-    width: 100%;
-    padding-bottom: 60%;
-    iframe {
-      top: 50%;
-      left: 50%;
-      width: 100%;
-      height: 100%;
-      transform: translate(-50%, -50%);
-      position: absolute;
+    height: 100%;
+    background: rgba(#000, 0.5);
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    transition: 0.3s;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .window-wrapper {
+        width: 100%;
+        max-width: 576px;
+        padding: 0 5px;
     }
-  }
+    .window {
+        position: relative;
+        width: 100%;
+        padding-bottom: 60%;
+        iframe {
+            top: 50%;
+            left: 50%;
+            width: 100%;
+            height: 100%;
+            transform: translate(-50%, -50%);
+            position: absolute;
+        }
+    }
 }
-  
 </style>

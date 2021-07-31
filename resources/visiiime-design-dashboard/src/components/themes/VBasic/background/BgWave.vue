9d9485591d303dad429746498e6f33aef73cd172
東東 <s8809880988@gmail.com>
2021-08-01 01:58:20 +0800

    dong-update

--- resources/visiiime-design-dashboard/src/components/themes/VBasic/background/BgWave.vue
+++ resources/visiiime-design-dashboard/src/components/themes/VBasic/background/BgWave.vue
@@ -1,81 +1,61 @@
 <template>
-  <div class="bgImage">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-    >
-      <symbol id="patch" viewBox="0 0 56 28" :fill="this.background.bgColor2">
-        <path
-          d="M56 26v2h-7.75c2.3-1.27 4.94-2 7.75-2zm-26 2a2 2 0 1 0-4 0h-4.09A25.98 25.98 0 0 0 0 16v-2c.67 0 1.34.02 2 .07V14a2 2 0 0 0-2-2v-2a4 4 0 0 1 3.98 3.6 28.09 28.09 0 0 1 2.8-3.86A8 8 0 0 0 0 6V4a9.99 9.99 0 0 1 8.17 4.23c.94-.95 1.96-1.83 3.03-2.63A13.98 13.98 0 0 0 0 0h7.75c2 1.1 3.73 2.63 5.1 4.45 1.12-.72 2.3-1.37 3.53-1.93A20.1 20.1 0 0 0 14.28 0h2.7c.45.56.88 1.14 1.29 1.74 1.3-.48 2.63-.87 4-1.15-.11-.2-.23-.4-.36-.59H26v.07a28.4 28.4 0 0 1 4 0V0h4.09l-.37.59c1.38.28 2.72.67 4.01 1.15.4-.6.84-1.18 1.3-1.74h2.69a20.1 20.1 0 0 0-2.1 2.52c1.23.56 2.41 1.2 3.54 1.93A16.08 16.08 0 0 1 48.25 0H56c-4.58 0-8.65 2.2-11.2 5.6 1.07.8 2.09 1.68 3.03 2.63A9.99 9.99 0 0 1 56 4v2a8 8 0 0 0-6.77 3.74c1.03 1.2 1.97 2.5 2.79 3.86A4 4 0 0 1 56 10v2a2 2 0 0 0-2 2.07 28.4 28.4 0 0 1 2-.07v2c-9.2 0-17.3 4.78-21.91 12H30zM7.75 28H0v-2c2.81 0 5.46.73 7.75 2zM56 20v2c-5.6 0-10.65 2.3-14.28 6h-2.7c4.04-4.89 10.15-8 16.98-8zm-39.03 8h-2.69C10.65 24.3 5.6 22 0 22v-2c6.83 0 12.94 3.11 16.97 8zm15.01-.4a28.09 28.09 0 0 1 2.8-3.86 8 8 0 0 0-13.55 0c1.03 1.2 1.97 2.5 2.79 3.86a4 4 0 0 1 7.96 0zm14.29-11.86c1.3-.48 2.63-.87 4-1.15a25.99 25.99 0 0 0-44.55 0c1.38.28 2.72.67 4.01 1.15a21.98 21.98 0 0 1 36.54 0zm-5.43 2.71c1.13-.72 2.3-1.37 3.54-1.93a19.98 19.98 0 0 0-32.76 0c1.23.56 2.41 1.2 3.54 1.93a15.98 15.98 0 0 1 25.68 0zm-4.67 3.78c.94-.95 1.96-1.83 3.03-2.63a13.98 13.98 0 0 0-22.4 0c1.07.8 2.09 1.68 3.03 2.63a9.99 9.99 0 0 1 16.34 0z"
-        />
-      </symbol>
-      <pattern
-        width="56"
-        height="28"
-        patternUnits="userSpaceOnUse"
-        id="pattern"
-        viewBox="0 0 56 28"
-      >
-        <use
-          xlink:href="#patch"
-          width="56"
-          height="28"
-          x="0"
-          y="0"
-          style="overflow: visible"
-        />
-        <!--     <use xlink:href="#patch"  width="50" height="50" x="0" y="0" style="overflow:visible;" transform="rotate(90,50,50)"/>
-    <use xlink:href="#patch"  width="50" height="50" x="0" y="0" style="overflow:visible;" transform="rotate(180,50,50)"/>
-    <use xlink:href="#patch"  width="50" height="50" x="0" y="0" style="overflow:visible;" transform="rotate(270,50,50)"/> -->
-      </pattern>
+    <div :class="bgClass">
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <symbol :id="`patch${uid}`" viewBox="0 0 56 28" :fill="background.bgColor2">
+                <path
+                    d="M56 26v2h-7.75c2.3-1.27 4.94-2 7.75-2zm-26 2a2 2 0 1 0-4 0h-4.09A25.98 25.98 0 0 0 0 16v-2c.67 0 1.34.02 2 .07V14a2 2 0 0 0-2-2v-2a4 4 0 0 1 3.98 3.6 28.09 28.09 0 0 1 2.8-3.86A8 8 0 0 0 0 6V4a9.99 9.99 0 0 1 8.17 4.23c.94-.95 1.96-1.83 3.03-2.63A13.98 13.98 0 0 0 0 0h7.75c2 1.1 3.73 2.63 5.1 4.45 1.12-.72 2.3-1.37 3.53-1.93A20.1 20.1 0 0 0 14.28 0h2.7c.45.56.88 1.14 1.29 1.74 1.3-.48 2.63-.87 4-1.15-.11-.2-.23-.4-.36-.59H26v.07a28.4 28.4 0 0 1 4 0V0h4.09l-.37.59c1.38.28 2.72.67 4.01 1.15.4-.6.84-1.18 1.3-1.74h2.69a20.1 20.1 0 0 0-2.1 2.52c1.23.56 2.41 1.2 3.54 1.93A16.08 16.08 0 0 1 48.25 0H56c-4.58 0-8.65 2.2-11.2 5.6 1.07.8 2.09 1.68 3.03 2.63A9.99 9.99 0 0 1 56 4v2a8 8 0 0 0-6.77 3.74c1.03 1.2 1.97 2.5 2.79 3.86A4 4 0 0 1 56 10v2a2 2 0 0 0-2 2.07 28.4 28.4 0 0 1 2-.07v2c-9.2 0-17.3 4.78-21.91 12H30zM7.75 28H0v-2c2.81 0 5.46.73 7.75 2zM56 20v2c-5.6 0-10.65 2.3-14.28 6h-2.7c4.04-4.89 10.15-8 16.98-8zm-39.03 8h-2.69C10.65 24.3 5.6 22 0 22v-2c6.83 0 12.94 3.11 16.97 8zm15.01-.4a28.09 28.09 0 0 1 2.8-3.86 8 8 0 0 0-13.55 0c1.03 1.2 1.97 2.5 2.79 3.86a4 4 0 0 1 7.96 0zm14.29-11.86c1.3-.48 2.63-.87 4-1.15a25.99 25.99 0 0 0-44.55 0c1.38.28 2.72.67 4.01 1.15a21.98 21.98 0 0 1 36.54 0zm-5.43 2.71c1.13-.72 2.3-1.37 3.54-1.93a19.98 19.98 0 0 0-32.76 0c1.23.56 2.41 1.2 3.54 1.93a15.98 15.98 0 0 1 25.68 0zm-4.67 3.78c.94-.95 1.96-1.83 3.03-2.63a13.98 13.98 0 0 0-22.4 0c1.07.8 2.09 1.68 3.03 2.63a9.99 9.99 0 0 1 16.34 0z"
+                />
+            </symbol>
+            <pattern width="56" height="28" patternUnits="userSpaceOnUse" :id="`pattern${uid}`" viewBox="0 0 56 28">
+                <use :xlink:href="`#patch${uid}`" width="56" height="28" x="0" y="0" style="overflow: visible;" />
+            </pattern>
 
-      <pattern id="double" xlink:href="#pattern"></pattern>
+            <pattern :id="`double${uid}`" :xlink:href="`#pattern${uid}`"></pattern>
 
-      <rect width="100%" height="100%" :fill="background.bgColor"></rect>
-      <rect width="100%" height="100%" fill="url(#double)"></rect>
-    </svg>
-  </div>
+            <rect width="100%" height="100%" :fill="background.bgColor"></rect>
+            <rect width="100%" height="100%" :fill="`url(#double${uid})`"></rect>
+        </svg>
+    </div>
 </template>
 
-
 <script>
-import { lightenColor, convertHexToRGBA, hexToHSL } from '../../../../helper/colorHelper'
-  export default {
+import uuid from '../../../../../src/helper/uuid'
+export default {
     props: {
-      background: {
-        type: Object,
-        required: true,
-      },
+        background: {
+            type: Object,
+            required: true,
+        },
+        isDemo: {
+            type: Boolean,
+            required: true
+        }
     },
     computed: {
-        lineColor() {
+        uid() {
+            return uuid();
+        },
+        bgClass() {
             return {
-                '--bg-color': hexToHSL(this.background.bgColor)
+                [this.$style['bgImage']]: true
             }
         }
     }
-  };
+};
 </script>
 
-<style lang="scss" scoped>
-$color: var(--bg-color);
-#patch {
-    // fill: darken(#aaa, 10%);
-    // fill: $color;
-}
+<style lang="scss" module>
 .bgImage {
-  position: absolute;
-  top: 0;
-  left: 50%;
-  transform: translateX(-50%);
-  width: 100%;
-  height: 100%;
-  z-index: -1;
-  background-position: center center;
-  background-size: cover;
-  svg {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
     width: 100%;
     height: 100%;
-  }
+    z-index: -1;
+    svg {
+        width: 100%;
+        height: 100%;
+    }
 }
 </style>

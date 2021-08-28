3ad960a2446991a76d2a0e48581fece049178d34
Mike <a3698521tw11@gmail.com>
2021-08-28 14:58:19 +0800

    feat(vBlog): add migrations

---
+++ database/migrations/2021_08_21_103300_create_v_tags_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('v_tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('名稱');
+            $table->uuidMorphs('taggable');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('v_tags');
+    }
+}

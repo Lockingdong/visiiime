14cc4805bd434aa067605fa0f4685b2155ee31d8
東東 <s8809880988@gmail.com>
2021-08-30 21:44:27 +0800

    dong update

--- resources/views/components/web/layouts/main.blade.php
+++ resources/views/components/web/layouts/main.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Visiiime | {{ $title }}</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.7/tailwind.min.css" integrity="sha512-y6ZMKFUQrn+UUEVoqYe8ApScqbjuhjqzTuwUMEGMDuhS2niI8KA3vhH2LenreqJXQS+iIXVTRL2iaNfJbDNA1Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    @include('components.web.layouts.colors')
     @yield('style')
 </head>
 <body>

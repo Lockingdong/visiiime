6c53e22a200fbd1e3ed6f59c48d7beb8db6d61a2
himi <himi@onmore.com.tw>
2021-09-25 17:29:53 +0800

    fix:v-page/list

--- resources/views/components/vPage/page-list.blade.php
+++ resources/views/components/vPage/page-list.blade.php
@@ -1,35 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.7/tailwind.min.css" integrity="sha512-y6ZMKFUQrn+UUEVoqYe8ApScqbjuhjqzTuwUMEGMDuhS2niI8KA3vhH2LenreqJXQS+iIXVTRL2iaNfJbDNA1Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
 </head>
-<body>
 
-<section class="py-20">
-  <div class="container px-4 mx-auto">
-    <div class="max-w-2xl mx-auto text-center">
-      <div class="max-w-md mb-8 mx-auto">
-        <span class="text-sm text-blueGray-400">Link URL</span>
-        <h2 class="mt-2 text-4xl font-bold font-heading">我的網域</h2>
-      </div>
-      @foreach ($pageList as $page)
-      <div class="mb-4">
-        <label class="text-left block text-blueGray-800 text-sm font-semibold mb-2" for="">Label for text</label> 
-        <div class="flex px-2 bg-blueGray-50 rounded">
-          <input class="appearance-none p-4 text-xs font-semibold leading-none bg-blueGray-50 rounded outline-none" type="text" name="field-name" placeholder="{{ $page->page_url }}">
-          <a href="{{ route('vPage.pageDesign', ['page_id' => $page->id]) }}" class="ml-auto inline-block py-4 px-8 text-xs text-white text-center font-semibold leading-none bg-blue-600 hover:bg-blue-700 rounded">編輯</a> 
+<body class="py-20 bg-gray-50"> 
+    <section>
+        <div class="container px-10 mx-auto">
+            <table class="bg-white rounded shadow mx-auto w-full max-w-2xl">
+                <thead>
+                    <tr class="text-left text-xs">
+                        <th class="px-4 py-4 font-normal w-auto">＃</th>
+                        <th class="px-4 py-4 font-normal w-2/3">網域</th>
+                        <th class="px-4 py-4 font-normal w-auto">操作</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($pageList as $page)
+                    <tr class="border-t border-blueGray-100">
+                        <td class="px-4 py-2 text-xs font-semibold">
+                            <span class="flex w-8 h-8 items-center justify-center text-xs rounded-full bg-blue-50 text-blue-600">1</span>
+
+                        </td>
+
+                        <td class="flex px-6 py-4 text-xs">
+                            <img class="w-8 h-8 -ml-2 border border-white rounded-full object-top object-cover" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=128&amp;q=60">
+                            <div class="pl-4">
+                                <p class="font-semibold">Irene</p>
+                                <a class="text-blueGray-400" href="#">
+                                    www.reactjs.org
+                                    <span class="text-blue-600">/ Irene</span>
+                                </a>
+                            </div>
+                        </td>
+
+                        <td class="py-2 px-2 sm:py-3 sm:px-6 text-xs">
+                            <a class="w-full md:w-auto py-2 px-2 sm:py-3 sm:px-6 text-xs text-white font-semibold leading-none bg-blue-600 hover:bg-blue-700 rounded" href="{{ route('vPage.pageDesign', ['page_id' => $page->id]) }}">編輯</a>
+                        </td>
+
+
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
-      </div>
+    </section>
 
-      @endforeach
-
-     
-    </div>
-  </div>
-</section>
     <!-- <div>
         <ul>
             @foreach ($pageList as $page)
@@ -40,4 +63,5 @@
         </ul>
     </div> -->
 </body>
+
 </html>

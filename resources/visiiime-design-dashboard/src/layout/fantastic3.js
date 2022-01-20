1f0373f9b3fec6747383ee6dcb505fb11c5e9998
himi <himi@onmore.com.tw>
2022-01-20 18:37:51 +0800

    feat: add bg style

---
+++ resources/visiiime-design-dashboard/src/layout/fantastic3.js
@@ -0,0 +1,20 @@
+export default {
+    background: {
+        customBgOn: true, // 不用動
+        bgType: "", // 不用動
+        bgName: "bgGradient", // 不用動
+        bgImage: "", // 不用動
+        bgColor: "#ddd6f3",
+        bgColor2: "#faaca8"
+    },
+    linkButton: {
+        buttonName: "", // 不用動
+        buttonBorder: "md-border", // reference: resources/visiiime-design-dashboard/src/components/themes/VBasic/layout/customLink/border/index.scss
+        buttonRadius: "sm-bdrs", // resources/visiiime-design-dashboard/src/components/themes/VBasic/layout/customLink/radius/index.scss
+        buttonBgColor: "transparent",
+        buttonTextColor: "#ffffff"
+    },
+    text: {
+        textColor: "#333333"
+    }
+}
